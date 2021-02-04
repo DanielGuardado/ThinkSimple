a673b0096a0f3411af8d1d554c5ab5b3ca8bc6e9
DanielGuardado <59269194+DanielGuardado@users.noreply.github.com>
2021-02-03 20:34:51 -0500

    added notes for future

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import HealthFormView from "./HealthFormView/HealthFormView";
@@ -6,12 +6,14 @@ import DocumentationView from "./DocumentationView/DocumentationView";
 import CompletionView from "./CompletionView/CompletionView";
 
 export default function App() {
+  const [formStatus, setFormStatus] = useState(false);
   return (
     <div className="App">
       {/* NOTE: Comment out DocumentationView before submission  */}
       {/* <DocumentationView /> */}
-      {/* <HealthFormView /> */}
-      {<CompletionView />}
+      {!formStatus ? <HealthFormView /> : <CompletionView />}
+      {/* <HealthFormView />
+      {<CompletionView />} */}
     </div>
   );
 }
--- src/components/HealthFormView/HealthFormView.js
+++ src/components/HealthFormView/HealthFormView.js
@@ -9,6 +9,11 @@ export default function HealthFormView() {
   return (
     <>
       {/* NOTE: Comment out DocumentationView before submission  */}
+      {/* to potentially solve how to make sure all of the fields are inputted
+      i could have validations on each checkbox and also make sure to send
+      back some type of boolean indicating wether or not that box is checked
+      and then enable the submit button only once each field has a true value
+      for each option that is required. */}
       <TripSurvey />
       <TravelerData />
       <TripSurveyQuestions />
